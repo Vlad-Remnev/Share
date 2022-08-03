d477f515ae7796cf65f5605a21d01c1e7e28a394
Vladislav Remnev <remnevvlad30@gmail.com>
2022-08-03 19:28:32 +0300

    1

--- src/App.tsx
+++ src/App.tsx
@@ -24,6 +24,7 @@ function App() {
             .then(json => setGet(json))
     }, [])
 
+
     return (
         <div className="App">
             <Button nickName={'Clean page'} callback={getRequestHandler}/>
